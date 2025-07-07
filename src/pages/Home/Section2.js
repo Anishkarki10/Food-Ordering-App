05eba7ac436f0c7f3779a15340fd0fc9dc5ba0f3
AnishKarki <karkianish141@gmail.com>
2025-07-07 20:12:49 +0545

    Update Section2.js

--- src/pages/Home/Section2.js
+++ src/pages/Home/Section2.js
@@ -10,18 +10,16 @@ const mockData = [
   {
     image: Pizza,
     title: "Original",
-    paragraph: `Porta semper lacus cursus, feugiat primis ultrice a ligula risus auctor an tempus feugiat dolor lacinia cubilia curae integer orci congue and metus integer primis in integer metus`,
+   
   },
   {
     image: Salad,
     title: "Qualty Foods",
-    paragraph: `Porta semper lacus cursus, feugiat primis ultrice a ligula risus auctor an tempus feugiat dolor lacinia cubilia curae integer orci congue and metus integer primis in integer metus`,
-  },
+    
   {
     image: Delivery,
     title: "Fastest Delivery",
-    paragraph: `Porta semper lacus cursus, feugiat primis ultrice a ligula risus auctor an tempus feugiat dolor lacinia cubilia curae integer orci congue and metus integer primis in integer metus`,
-  },
+   
   // Add more mock data objects as needed
 ];
 
